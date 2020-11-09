8ac1d254cee062c31cce02999ec6efafe6f51d24
Marcin-Stepniak <marcinstrpniak.rad@gmail.com>
2020-11-09 19:45:21 +0100

    Add initial files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import * as React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function IconAntennaBars5({
+  size = 24,
+  color = "currentColor",
+  stroke = 2,
+  ...props
+}) {
+  return <svg className="icon icon-tabler icon-tabler-antenna-bars-5" width={size} height={size} viewBox="0 0 24 24" strokeWidth={stroke} stroke={color} fill="none" strokeLinecap="round" strokeLinejoin="round" {...props}>
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <line id="line1" x1={6} y1={18} x2={6} y2={15} />
+    <line id="line2" x1={10} y1={18} x2={10} y2={12} />
+    <line id="line3" x1={14} y1={18} x2={14} y2={9} />
+    <line id="line4" x1={18} y1={18} x2={18} y2={6} /></svg>;
 }
 
-export default App;
+export default IconAntennaBars5;
